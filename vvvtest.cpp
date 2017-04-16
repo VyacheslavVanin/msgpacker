370bacb6ab9afcdb18fb8d0abce60f2757c92427
Viktor Vanin <vik.vanin@gmail.com>
2017-04-16 14:47:44 +0300

    reimplement vvvtest module

--- vvvtest.cpp
+++ vvvtest.cpp
@@ -1,26 +1,9 @@
 #include "vvvtest.h"
-#include <vector>
 
-
-namespace __test_private {
-std::vector<test_func_t>& __tests_registred()
-{
-    static std::vector<test_func_t> tests;
-    return tests;
-}
-
-__test_register::__test_register(test_func_t func)
-{
-    __tests_registred().push_back(func); 
-}
-}
-
-bool testall()
-{
-    const std::vector<test_func_t>& tests = __test_private::__tests_registred();
+bool testall(const std::vector<const __TesterBase*>& tests) {
     int ret = 0;
     for(size_t i = 0; i < tests.size(); ++i) {
-        ret += tests[i]();
+        ret += tests[i]->test();
     }
     return ret == 0;
 }
