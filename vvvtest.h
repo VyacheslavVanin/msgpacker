a3129478a6a266d9e838fad2ee95bab07ba52d74
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2017-04-16 10:34:07 +0300

    vvvtest: remove name from test declaration interface

--- vvvtest.h
+++ vvvtest.h
@@ -18,16 +18,27 @@ class __test_register
 };
 }
 
-#define VVVTEST_EQ(name, expr) \
+
+#define PASTER(x, y) x##y
+#define EVALUATOR(x, y) PASTER(x, y)
+#define VVVTEST_EQ_(name, expr) \
+namespace { \
 int name() \
 {\
     printf("test: " #expr " ... ");\
     if((expr)) {printf("passed.\n"); return  0;} \
     else       {printf("failed.\n"); return -1;} \
 } \
-namespace __test_private { \
-__test_register name##object = __test_register(name); \
+\
+__test_private::__test_register EVALUATOR(name,object) = \
+                        __test_private::__test_register(name); \
+\
 }
+#define NAME(x) EVALUATOR(x, __LINE__)
+#define NAME_(expr) VVVTEST_EQ_(NAME(__vvvtest_), expr)
+#define VVVTEST_EQ(expr) NAME_(expr)
+
+
 
 #endif
 
